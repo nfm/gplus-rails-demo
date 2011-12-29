f91af0fe21927fbd700ff36869ff657883230927
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-12-30 08:30:26 +1100

    Define #setup_gplus in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def setup_gplus
+    @gplus = Gplus::Client.new(
+      :api_key => GPLUS_CONFIG['api_key'],
+      :client_id => GPLUS_CONFIG['client_id'],
+      :client_secret => GPLUS_CONFIG['client_secret'],
+      :redirect_uri => GPLUS_CONFIG['redirect_uri']
+    )
+  end
 end
