41d5ba64c061aa76265bc5174cd7632bf391d11a
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-12-30 09:05:02 +1100

    Define #setup_authorized_gplus in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,16 @@ class ApplicationController < ActionController::Base
       :redirect_uri => GPLUS_CONFIG['redirect_uri']
     )
   end
+
+  def setup_authorized_gplus(user)
+    @gplus = Gplus::Client.new(
+      :token => user.token,
+      :refresh_token => user.refresh_token,
+      :token_expires_at => user.token_expires_at,
+      :api_key => GPLUS_CONFIG['api_key'],
+      :client_id => GPLUS_CONFIG['client_id'],
+      :client_secret => GPLUS_CONFIG['client_secret'],
+      :redirect_uri => GPLUS_CONFIG['redirect_uri']
+    )
+  end
 end
