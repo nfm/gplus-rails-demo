1104259d3b809229b8c65a0badce3fdbc8357c12
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-12-30 08:22:24 +1100

    Generate Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GplusRailsDemo::Application.config.secret_token = '3750f0eb99f1e4b9193aae328433c6bb990000abd3dfc6cc21f5c8ee1336b48f467d5c02afe8fc81f26adaff82939ab6fe6698684e73675ef5c328897e4d51d6'
