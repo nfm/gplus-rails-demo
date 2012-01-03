7d86caa99d8bdf8c40de799e42b8c225a390d198
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2012-01-04 08:42:51 +1100

    Refactor setup_authorized_gplus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,20 +13,13 @@ class ApplicationController < ActionController::Base
 
   # Setup a Gplus::Client authorized to use the specified user's stored OAuth token
   def setup_authorized_gplus(user)
-    @gplus = Gplus::Client.new(
-      :token => user.token,
-      :refresh_token => user.refresh_token,
-      :token_expires_at => user.token_expires_at,
-      :api_key => GPLUS_CONFIG['api_key'],
-      :client_id => GPLUS_CONFIG['client_id'],
-      :client_secret => GPLUS_CONFIG['client_secret'],
-      :redirect_uri => GPLUS_CONFIG['redirect_uri']
-    )
+    setup_gplus
+    access_token = @gplus.authorize(user.token, user.refresh_token, user.token_expires_at)
 
     # Check if the access token was implicitly refreshed
     # If it was, store the user's new token and the time that it expires at
     if @gplus.access_token_refreshed?
-      user.update_attributes(:token => @gplus.access_token.token, :token_expires_at => @gplus.access_token.expires_at)
+      user.update_attributes(:token => access_token.token, :token_expires_at => access_token.expires_at)
     end
   end
 end
